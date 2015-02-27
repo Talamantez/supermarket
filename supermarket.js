87f8428372c4504a36d1632e1f993c4be3eaa020
Talamantez <robert.talamantez2@gmail.com>
2015-02-26 16:57:33 -0800

    Added check to prevent duplication in product naming

--- supermarket.js
+++ supermarket.js
@@ -7,9 +7,11 @@ supermarket.js exports a Supermarket object
 List of Properties:
     
     cart: A String wherein each char is the name of a product
-    products: A list of product objects that correspond to the chars in the cart string
+    products: An array of product objects that correspond to the chars in the cart string
 
 List of Methods:
+
+    Product(): Product constructor
 
     checkout(): Counts up products, sums their prices, applies discounts, and returns a Total
     countProduct(): Count up instances of a product 
@@ -17,15 +19,21 @@ List of Methods:
     
     initCart(): Initialize cart as the String 'ABBACBBAB', 
             or uses argument from commandline (i.e. node checkout.js 'CCCCBBAAA') 
-    Product(): Product constructor
-
+    initProducts(): Creates Products named 'A', 'B', and 'C' and pushes them to products[];
+    init(): Runs initCart() and initProducts()
+    
     findProduct(): Locate a product by name
     updatePrice(): Update a product's price
+
 */
 
 var _ = require('./underscore');
 
 var Supermarket = function(){
+    
+    // Allow nested access to object
+    var self = this;
+
     this.cart = '';
     this.Product = function( name, price, discountThresh, discountAmt ){
         this.name = name;
@@ -39,15 +47,15 @@ var Supermarket = function(){
 
     this.checkout = function( items ){
 
-        // Allow access to 'this' from local scope
-        var that = this;
         // Initialize total
         var total = 0;
+        
         // Loop over products array
-        _.each( that.products, function( product ){
+        _.each( self.products, function( product ){
             console.log('\nSumming '+ product.name + ' products');
+        
             // Count instances of product in input string
-            var numItems = that.countProduct( product.name );
+            var numItems = self.countProduct( product.name );
             
             /* 
             Multiply the product's price by count of instances
@@ -55,32 +63,35 @@ var Supermarket = function(){
             */
 
             total += numItems * product.price;
-            console.log('updated total: ' + total);          
+            console.log('Subtotal : $' + total);          
+        
             /* 
             If a product has discount data and the count of instances 
-            of the item is greaterthan its discount threshold, 
-            count up the number of discounts and apply them to the total
+            of the item is greater than its discount threshold, 
+            count up the number of discounts, sum them, and subtract 
+            them from the total
             */
             if(product.discountThresh && product.discountAmt){
                 if( numItems/product.discountThresh >= 1){
                     var numDiscounts = Math.floor( numItems/product.discountThresh );
-                    total = that.applyDiscount( total, product.discountAmt , numDiscounts );
-                    console.log('discount applied: ' + total);
+                    total = self.applyDiscount( total, product.discountAmt , numDiscounts );
+                    console.log('Discount applied, Subtotal: $' + total);
                 }
             }
         });
     
         // return the total cost for the cart
+        console.log('\nGRAND TOTAL FOR CART:  $' + total +'\n' );
         return total;
-  }
+    }
 
     this.countProduct = function( productName ){
 
         // Split the input string into an array
-        var itemsArray = this.cart.split('');
+        var itemsArray = self.cart.split('');
 
         // Filter based on product name
-        var filteredArray = _.filter( this.cart , function(char){ return char === productName});
+        var filteredArray = _.filter( self.cart , function(char){ return char === productName});
         
         // Return the length of the array
         return( filteredArray.length );
@@ -103,15 +114,17 @@ var Supermarket = function(){
 
         // Put some items in the cart
         this.cart += process.argv[2] || 'ABBACBBAB';
-        console.log( 'Cart initialized ' + this.cart );
+        console.log( 'Cart initialized ' + self.cart );
     }
     this.initProducts = function(){
 
         // Put some Products in the products array
-        this.products.push( new this.Product( 'A' , 20.00 ) ,
-                            new this.Product( 'B' , 50.00 , 5 , 100.00 ) ,
-                            new this.Product( 'C' , 30.00 ) 
-        );
+            self.addProduct( 'A' , 20.00 );
+            self.addProduct( 'B' , 50.00 , 5 , 100.00 );
+            self.addProduct( 'C' , 30.00 ); 
+
+        // Attempt to add another Product named 'A', expect to reject
+            self.addProduct( 'A' , 20.00 );
     }
     this.init = function(){
 
@@ -121,18 +134,40 @@ var Supermarket = function(){
         this.initProducts();
     }
     this.updatePrice = function( productName , newPrice ){
-        var updatee = this.findProduct( productName );
+        var updatee = self.findProduct( productName );
         console.log('\nupdating ' + updatee.name );
         updatee.price = newPrice;
         console.log('\nNew price for ' + updatee.name + ' is ' + newPrice );
     }
     this.findProduct = function( productName ){
-        console.log('\nfinding ' + productName );
-        var foundProduct = _.find(this.products, function( obj ){
+        console.log('\nFinding ' + productName );
+        var foundProduct = _.find(self.products, function( obj ){
             return obj.name ===  productName
         });
-        return foundProduct;
+        if( !foundProduct ){
+            console.log('Product not found ');
+            return;
+        } else {
+            return foundProduct;
+        }
     }
+    this.addProduct = function( name, price, discountThresh, discountAmt ){
+     
+
+        if( name && price ){
+              if( self.findProduct( name ) ){
+                console.log('That product is already in the inventory');
+                return;
+            } else {
+                  var newProduct = new self.Product( name, price, discountThresh, discountAmt );
+                  self.products.push( newProduct );
+            }
+        } else {
+            console.log('A name and price are required to add a product');
+            return;
+        }
+    }
+
 }
 
 module.exports = Supermarket;
