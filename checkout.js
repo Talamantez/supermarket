3b06025705ed19c5fab87b514cad0434b6150529
Talamantez <robert.talamantez2@gmail.com>
2015-02-26 17:07:41 -0800

    added documentation in checkout.js

--- checkout.js
+++ checkout.js
@@ -1,6 +1,17 @@
+/* 
+ *  checkout.js is run with Node. 
+ *  Usage: node checkout <optional product string>
+ *  By default, it will initialize a Supermarket object,
+ *  its products and cart items as specified in the code-challenge,
+ *  then run 'checkout' and return the total cost of the cart.
+ *
+ *  The optional product string should be a series of capital A's,
+ *  B's and C's. (i.e. 'AAABBBCCC' or 'CCCCBAABA')
+*/
+
 'use strict'
 
-var S = require('./supermarket');
-var s = new S();
-s.init();
-s.checkout();
+var Supermarket = require('./supermarket');
+var myMarket = new Supermarket();
+myMarket.init();
+myMarket.checkout();
